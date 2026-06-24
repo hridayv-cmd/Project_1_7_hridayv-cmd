94cbcdfa2f8bf9babb0f574afe905fb7b937cd54
Hriday <hridayvermani12@gmail.com>
2026-06-24 09:41:36 -0400

    Add variables and simple data types also add empty list

--- platformer_adventure.py
+++ platformer_adventure.py
@@ -4,4 +4,11 @@ Author: Hriday Vermani
 Purpose: Simple text-based platformer style game using only Chapters 1-7 concepts.
 Date: June 23 2026
 """
-
+# === Variables & Simple Data Types ===
+PLAYER_NAME = "Mario"           # String
+HEALTH = 100                    # Integer
+SCORE = 0                       # Integer
+CURRENT_LEVEL = 1               # Integer
+GAME_RUNNING = True             # Boolean
+# List - stores player's collected items (ordered)
+inventory = []
