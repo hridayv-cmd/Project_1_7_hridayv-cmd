31e731801ed250d01a539dd84da9cc92909c3eb6
Hriday <hridayvermani12@gmail.com>
2026-06-24 12:12:56 -0400

    Add variable and data types

--- Zombie_apocalyspe.py
+++ Zombie_apocalyspe.py
@@ -4,3 +4,9 @@ Author: Hriday Vermani
 Purpose: Simple text-based zombie apocalsype game using Chapters 1-7 concepts.
 Date: June 23 2026
 """
+# 1. Variables & Simple Data Types
+NAME = "Base One"
+days = 0
+scrap = 40
+integrity = 100
+running = True
